3fc7731e2e6106d2afa8be267ab43bfcf6fc7613
Patrik Gaicean <patrik@nassautech.xyz>
2024-11-21 21:12:59 +0200

    rewrite generateShortUrl logic

--- persistence/urls.js
+++ persistence/urls.js
@@ -6,10 +6,13 @@ import { connectDB } from "./db.js";
  */
 export async function getShortUrl(longUrl) {
     const db = await connectDB();
+
+    /**
+     * @type {Collection} collection
+     */
     const collection = db.collection('urls');
 
     const doc = await collection.findOne({ longUrl }, { projection: { shortUrl: 1 } });
-
     if (!doc) {
         return await generateShortUrl(longUrl, collection)
     }
@@ -33,15 +36,20 @@ export async function redirectShortUrl(shortUrl) {
  * @param {Collection} collection 
  */
 async function generateShortUrl(longUrl, collection) {
+    let gen;
+    let doc;
+
+    do {
+        gen = generateUrl();
+        doc = await collection.findOne({ shortUrl: gen }, { projection: { shortUrl: 1 } });
+    } while (doc);
+
+    await collection.insertOne({ shortUrl: gen, longUrl });
+
+    return gen;
+}
+
+function generateUrl(longUrl) {
     const gen = Math.floor(Math.random() * 100);
-
-    const doc = await collection.findOne({ shortUrl: gen }, { projection: { shortUrl: 1 } })
-
-    if (doc) {
-        return await generateShortUrl(longUrl, collection);
-    }
-
-    await collection.insertOne({ shortUrl: gen, longUrl })
-
     return gen;
 }
