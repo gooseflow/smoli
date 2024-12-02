3ebb0900ef9d4851951d72d4f9df0a983999e851
Patrik Gaicean <patrik@nassautech.xyz>
2024-12-02 13:19:04 +0200

    add tests for all persistence/urls functions

--- __tests__/persistence/urls.js
+++ __tests__/persistence/urls.js
@@ -1,4 +1,4 @@
-import { NotFoundError } from '../../http/errors.js';
+import { BadRequestError, NotFoundError } from '../../http/errors.js';
 import { closeDB, connectDB } from '../../persistence/db.js';
 import { urlsDb } from '../../persistence/urls.js';
 
@@ -22,7 +22,7 @@ describe('urlsDb', () => {
     describe('getLongUrl', () => {
         it('returns a long URL if it finds a document', async () => {
             const shortUrl = 'aaa111';
-            const longUrl = 'https://example.com/some-long-url';
+            const longUrl = 'https://example.com/some-long-url-aaa111';
             const id = await createUrlPair(longUrl, shortUrl);
 
             const result = await urlsDb.getLongUrl(shortUrl);
@@ -31,9 +31,67 @@ describe('urlsDb', () => {
         });
 
         it(`throws a NotFoundError if it doesn't find a document`, async () => {
-            const shortUrl = 'bbb222';
+            const shortUrl = 'aaa222';
 
             await expect(urlsDb.getLongUrl(shortUrl)).rejects.toThrow(NotFoundError);
+        });
+    });
+
+    describe('shortUrlExists', () => {
+        it('returns true if short URL exists', async () => {
+            const shortUrl = 'bbb111';
+            const longUrl = 'https://example.com/some-long-url-bbb111';
+            const id = await createUrlPair(longUrl, shortUrl);
+
+            const result = await urlsDb.shortUrlExists(shortUrl);
+            expect(result).toBe(true);
+            expect(await removeUrlPair(id)).toBe(true);
+        });
+
+        it('returns false if short URL does not exist', async () => {
+            const shortUrl = 'bbb222';
+
+            const result = await urlsDb.shortUrlExists(shortUrl);
+            expect(result).toBe(false);
+        });
+    });
+
+    describe('getShortUrl', () => {
+        it('returns a short URL if it finds a document', async () => {
+            const shortUrl = 'ccc111';
+            const longUrl = 'https://example.com/some-long-url-ccc111';
+            const id = await createUrlPair(longUrl, shortUrl);
+
+            const result = await urlsDb.getShortUrl(longUrl);
+            expect(result).toBe(shortUrl);
+            expect(await removeUrlPair(id)).toBe(true);
+        });
+
+        it(`throws a NotFoundError if it doesn't find a document`, async () => {
+            const longUrl = 'https://example.com/some-long-url-ccc222';
+
+            await expect(urlsDb.getShortUrl(longUrl)).rejects.toThrow(NotFoundError);
+        });
+    });
+
+    describe('createShortUrl', () => {
+        it('returns the insertedId of the URL pair', async () => {
+            const shortUrl = 'ddd111';
+            const longUrl = 'https://example.com/some-long-url-ddd111';
+
+            const result = await urlsDb.createShortUrl(longUrl, shortUrl);
+            expect(result).toHaveProperty('insertedId');
+            expect(await removeUrlPair(result.insertedId)).toBe(true);
+        });
+
+        it('throws a BadRequestError if longUrl is not unique in the db', async () => {
+            const shortUrl = 'ddd222';
+            const longUrl = 'https://example.com/some-long-url-ddd222';
+
+            const id = await createUrlPair(longUrl, shortUrl);
+
+            await expect(urlsDb.createShortUrl(longUrl, shortUrl)).rejects.toThrow(BadRequestError);
+            expect(await removeUrlPair(id)).toBe(true);
         });
     });
 
